a84867bced9d867747be4f080d23daf160074547
T00642089 <josueh0207@gmail.com>
2021-03-13 23:28:16 -0800

    number buttons now display the numbers in the result area

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -5,19 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.Stack;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Stack<Integer> myStack;
     private char myOperator;
+    private String numberText = "";
+    private TextView textView;
     private Button number0, number1, number2, number3, number4, number5, number6, number7, number8, number9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView_result);
 
         number0 = findViewById(R.id.button_0);
         number0.setOnClickListener(this);
@@ -48,7 +53,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         number9 = findViewById(R.id.button_9);
         number9.setOnClickListener(this);
-
     }
 
     @Override
@@ -56,5 +60,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
+        switch(v.getId())
+        {
+            case R.id.button_0:
+                if(numberText.length() != 0){
+                    numberText += number0.getText().toString() + "";
+                    textView.setText(numberText);
+                }
+                break;
+            case R.id.button_1:
+                    numberText += number1.getText().toString() + "";
+                    textView.setText(numberText);
+                break;
+            case R.id.button_2:
+                numberText += number2.getText().toString() + "";
+                textView.setText(numberText);
+                break;
+            case R.id.button_3:
+                numberText += number3.getText().toString() + "";
+                textView.setText(numberText);
+                break;
+            case R.id.button_4:
+                numberText += number4.getText().toString() + "";
+                textView.setText(numberText);
+                break;
+            case R.id.button_5:
+                numberText += number5.getText().toString() + "";
+                textView.setText(numberText);
+                break;
+            case R.id.button_6:
+                numberText += number6.getText().toString() + "";
+                textView.setText(numberText);
+                break;
+            case R.id.button_7:
+                numberText += number7.getText().toString() + "";
+                textView.setText(numberText);
+                break;
+            case R.id.button_8:
+                numberText += number8.getText().toString() + "";
+                textView.setText(numberText);
+                break;
+            case R.id.button_9:
+                numberText += number9.getText().toString() + "";
+                textView.setText(numberText);
+                break;
+
+
+        }
+
+
+
+
     }
 }
+
