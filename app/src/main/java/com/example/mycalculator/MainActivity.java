4625c19829b62209c32c4486c498f68be0d6f2ca
T00642089 <josueh0207@gmail.com>
2021-03-14 00:05:11 -0800

    functionality of add button added

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -11,11 +11,12 @@ import java.util.Stack;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private Stack<Integer> myStack;
+    private Stack<Integer> myStack = new Stack<Integer>();
     private char myOperator;
     private String numberText = "";
     private TextView textView;
     private Button number0, number1, number2, number3, number4, number5, number6, number7, number8, number9;
+    private Button addButton, subtractButton, multiplyButton, divideButton, deleteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,39 +27,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         number0 = findViewById(R.id.button_0);
         number0.setOnClickListener(this);
-
         number1 = findViewById(R.id.button_1);
         number1.setOnClickListener(this);
-
         number2 = findViewById(R.id.button_2);
         number2.setOnClickListener(this);
-
         number3 = findViewById(R.id.button_3);
         number3.setOnClickListener(this);
-
         number4 = findViewById(R.id.button_4);
         number4.setOnClickListener(this);
-
         number5 = findViewById(R.id.button_5);
         number5.setOnClickListener(this);
-
         number6 = findViewById(R.id.button_6);
         number6.setOnClickListener(this);
-
         number7 = findViewById(R.id.button_7);
         number7.setOnClickListener(this);
-
         number8 = findViewById(R.id.button_8);
         number8.setOnClickListener(this);
-
         number9 = findViewById(R.id.button_9);
         number9.setOnClickListener(this);
+
+        addButton = findViewById(R.id.button_add);
+        addButton.setOnClickListener(this);
+        subtractButton = findViewById(R.id.button_subtract);
+        subtractButton.setOnClickListener(this);
+        multiplyButton = findViewById(R.id.button_multiply);
+        multiplyButton.setOnClickListener(this);
+        divideButton = findViewById(R.id.button_divide);
+        divideButton.setOnClickListener(this);
+        deleteButton = findViewById(R.id.button_delete);
+        deleteButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
 
-
+        int i;
 
         switch(v.getId())
         {
@@ -103,6 +106,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.button_9:
                 numberText += number9.getText().toString() + "";
                 textView.setText(numberText);
+                break;
+            case R.id.button_add:
+                i = Integer.parseInt(numberText);
+
+                if(myStack.isEmpty())
+                {
+                    myStack.push(i);
+                    numberText = "";
+                    textView.setText("");
+                }
+                else
+                {
+                    int result = myStack.pop() + i;
+                    myStack.push(result);
+                    textView.setText("");
+
+                    numberText = result + "";
+                    textView.setText(numberText);
+                    numberText = "";
+                }
+                break;
+            case R.id.button_subtract:
+                i = Integer.parseInt(numberText);
+
+                if(myStack.isEmpty())
+                {
+                    myStack.push(i);
+                    numberText = "";
+                    textView.setText("");
+                }
+                else
+                {
+                    int result = myStack.pop() - i;
+                    myStack.push(result);
+                    textView.setText("");
+
+                    numberText = result + "";
+                    textView.setText(numberText);
+                    numberText = "";
+                }
                 break;
 
 
